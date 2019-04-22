b8a14586bea28f7a3a8c3d9b44f0f3ab14718d1f
chefei <969769904@qq.com>
2019-04-22 19:45:33 +0800

    Add files via upload

---
+++ tutorial/tutorial/spiders/quotes.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from ..items import QuoteItem
+
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'  # 每个项目唯一的名字，来区分不同的Spider
+    allowed_domains = ['quotes.toscrape.com']  # 允许爬的域名
+    start_urls = ['http://quotes.toscrape.com/']  # 启动时爬取的url列表
+
+     # 定义解析方法
+    def parse(self, response):
+        quotes = response.css('.quote')
+        for quote in quotes:
+            item = QuoteItem()
+            item['text'] = quote.css('.text::text').extract_first()
+            item['author'] = quote.css('.author::text').extract_first()
+            item['tags'] = quote.css('.tags .tag::text').extract()
+            yield item
+        next = response.css('.pager .next a::attr("href")').extract_first()
+        url = response.urljoin(next)
+        yield scrapy.Request(url=url, callback=self.parse)
